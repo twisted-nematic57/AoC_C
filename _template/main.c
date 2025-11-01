2783c03627b125e64ce22d5616124f21df052485
Akshat Singh <twisted.nematic57@gmail.com>
2025-11-01 13:35:10 -0700

    Finished character-based template

--- _template/main.c
+++ _template/main.c
@@ -3,15 +3,22 @@
 #include <stdlib.h>
 
 int main() {
-	FILE *inputf = fopen("input.txt","r");
-	if(inputf == NULL) {
-		fputs("FATAL: Error reading input.txt\n\n",stdout);
-		return 1;
-	}
+  FILE *inputf = fopen("input.txt","r");
+  if(inputf == NULL) {
+    perror("FATAL: Error reading input.txt\n\n");
+    return 1;
+  }
 
-	int a = fgetc(inputf);
-	fputc(a,stdout);
+  int currChar;
+  int result = 0; // Rename var & change type as needed
+  while((currChar = fgetc(inputf)) != EOF) {
+    fputc(currChar,stdout); // Sample code, change as needed
+    fputs("\n",stdout);
+    result++;
+  }
 
-	fclose(inputf);
-	return 0;
+  printf("\nResult: %d\n",result);
+  
+  fclose(inputf);
+  return 0;
 }
