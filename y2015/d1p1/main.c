7e018855f271ee6bf0fe3e9b0ca217b8ae0a84dc
Akshat Singh <twisted.nematic57@gmail.com>
2025-11-02 00:17:28 -0700

    2015 d1p1: 25 μs

--- y2015/d1p1/main.c
+++ y2015/d1p1/main.c
@@ -1,18 +1,26 @@
 // AoC 2025 - Day 1 Part 1
+// Fastest recorded execution: 25 μs
 #include <stdio.h>
 #include <stdlib.h>
-//#include <sys/mman.h>
+#include <sys/mman.h>
+#include <sys/unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <sys/time.h>
 
 //#define ENABLE_PRINTF_DBG true // Enable printf debugging output
 #define ENABLE_TIMING     true // Time the execution of the "meat" of the solution
 
 int main() {
-  FILE *inputf = fopen("input.txt","r");
-  if(inputf == NULL) {
+  int inputfd = open("input.txt",O_RDONLY); // Get file descriptor for input.txt
+  struct stat inputst;
+  if(inputfd == -1 || fstat(inputfd,&inputst) == -1) { // fstat to get size of file
     perror("FATAL: Error reading input.txt\n\n");
     return 1;
   }
+  char *inputbuf = mmap(NULL,inputst.st_size,PROT_READ,MAP_PRIVATE,inputfd,0); // Read into RAM!
+  close(inputfd);
 
   #ifdef ENABLE_PRINTF_DBG
   uint64_t step = 0;
@@ -25,14 +33,11 @@ int main() {
 
   /// --- BEGIN TIMING ---
 
-  int currChar, currFloor = 0;
-  while((currChar = fgetc(inputf)) != EOF) {
-    switch(currChar) {
-      case '(':
-        currFloor++;
-        break;
-      case ')':
-        currFloor--;
+  int currFloor = 0;
+  for(size_t i = 0; i < inputst.st_size; i++) {
+    switch(inputbuf[i]) {
+      case '(': currFloor++; break;
+      case ')': currFloor--;
     }
 
     #ifdef ENABLE_PRINTF_DBG
@@ -44,14 +49,14 @@ int main() {
   #ifdef ENABLE_TIMING
   gettimeofday(&t_end,NULL);
   /// --- END TIMING ---
-  uint64_t t_elapsed = (t_end.tv_sec - t_start.tv_sec) * 1000000 + (t_end.tv_usec - t_start.tv_usec);
-  printf("\nTime elapsed: %llu μs / %.3lf ms\n",t_elapsed,(double)(t_elapsed*.001));
+  uint32_t t_elapsed = (t_end.tv_sec - t_start.tv_sec) * 1000000 + (t_end.tv_usec - t_start.tv_usec);
+  printf("\nTime elapsed: %lu μs / %.3lf ms\n",t_elapsed,(double)(t_elapsed*.001));
   #else
   fputs("\n",stdout);
   #endif // ENABLE_TIMING
 
   printf("Final output: %d\n",currFloor);
-  
-  fclose(inputf);
+
+  munmap(inputbuf,inputst.st_size);
   return 0;
 }
